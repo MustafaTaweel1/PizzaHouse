96a6ea6d65ad1d98f19bcff7aa4c9725fef80f8b
MustafaTaweel1 <mostfapc@gmail.com>
2024-03-01 22:39:05 +0200

    add breeze for this

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\PizzaController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use app\Models\Pizza;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,17 +19,19 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-//middleware  dont open page if you not login 
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/pizzas',[PizzaController::class ,'index'])->name('pizzas.index')->middleware('auth');
-Route::post('/pizzas',[PizzaController::class,'store'])->name('pizzas.store');
-Route::get('/pizzas/create',[PizzaController::class ,'create'])->name('pizzas.create');
-Route::get('/pizzas/{id}',[PizzaController::class ,'show'])->name('pizzas.show'); 
+Route::post('/pizzas',[PizzaController::class,'store'])->name('pizzas.store')->middleware('auth');
+Route::get('/pizzas/create',[PizzaController::class ,'create'])->name('pizzas.create')->middleware('auth');
+Route::get('/pizzas/{id}',[PizzaController::class ,'show'])->name('pizzas.show')->middleware('auth'); 
 Route::delete('/pizzas/{id}',[PizzaController::class, 'delete'])->name('pizzas.delete')->middleware('auth');
 
-
-// reonve Route register in wensite and url
-Auth::routes([
-    'register'=>true
-]);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
